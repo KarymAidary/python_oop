30551836205864a6499beec5e2527430f3e5d527
Айдари Карим <karimaidariaskm@yandex.ru>
2018-06-25 15:29:53 +0300

    first commit

---
+++ baselist.py
@@ -0,0 +1,10 @@
+class Element(object):
+	"""docstring for Element"""
+	def __init__(self, coefficient, power, nexxt):
+		self.coefficient = coefficient
+		self.power = power
+		self.next = nexxt 
+		
+
+	def __str__(self):
+		pass
---
+++ lab1_1.py
@@ -0,0 +1,11 @@
+class Polynomial(object):
+	"""docstring for polynomial"""
+
+	def __init__(self, coefficient, power, nexxt):
+		self.coefficient = coefficient
+		self.power = power
+		self.next = nexxt 
+		
+
+	def __str__(self):
+		pass
