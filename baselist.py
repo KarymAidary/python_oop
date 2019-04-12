3c79291fe08c7221c239e55a50aec5cf0936e08f
Айдари Карим <karimaidariaskm@yandex.ru>
2019-04-12 14:56:01 +0300

    refactoring

--- baselist.py
+++ baselist.py
@@ -7,20 +7,31 @@ class BaseList:
         self.last = None
 
     def __getitem__(self, item):
-        if isinstance(item, int):
-            if item == 0:
-                return self.first
-            elif item == -1:    
-                return self.last
-            else:
-                element = self.first
-                counter = 0
-                while item !=counter:
-                    counter += 1
-                    element = element.next
-                return element
+        return self.get(item)
 
-    def __call__(self, *args, **kwargs):
+    def __repr__(self):
+        return self.show()
+        
+    def __delitem__(self, item_index):
+        self.remove(item_index) 
+
+    def __replace_item__(self, item, power, coefficient):
+        self.replace(item, power, coefficient)
+
+    @property
+    def list_lenght(self):
+        lenght = "[]"
+        if not self.first:
+            return lenght
+        else:
+            counter = 0
+            element = self.first
+            while element != None:
+                element = element.next
+                counter += 1
+            return counter
+
+    def show():
         if not self.first:
             return '[]'
         else:
@@ -35,42 +46,18 @@ class BaseList:
         list_elements_string = string_start + string_middle + "]"
         return '{}'.format(list_elements_string)
 
-    def __repr__(self):
-        return self.__call__()
 
-    def __len__(self):
-        lenght = "[]"
-        if not self.first:
-            return lenght
-        else:
-            counter = 0
-            element = self.first
-            while element != None:
-                element = element.next
-                counter += 1
-            return counter
-
-    def __delitem__(self, item_index):
+    def replace(self, item, power, coefficient):
+        previous_element = self.__getitem__(item - 1)
+        current_element = previous_element.next
+        new_element = Element(coefficient=coefficient, power=power, next=current_element.next)
+        previous_element.next = new_element  
+    
+    def remove(self, item_index):
         element = self.__getitem__(item_index - 1)
         element_to_delete = element.next
         next_element = element_to_delete.next
         element.next = next_element
-
-    def __replace_item__(self, item, power, coefficient):
-        previous_element = self.__getitem__(item - 1)
-        current_element = previous_element.next
-        new_element = Element(coefficient=coefficient, power=power, next=current_element.next)
-        previous_element.next = new_element    
-
-    @property
-    def list_lenght(self):
-        return self.__len__()
-
-    def replace(self, item, power, coefficient):
-        self.__replace_item__(item, power, coefficient)
-    
-    def remove(self, item_index):
-        self.__delitem__(item_index)          
 
     def add(self, coefficient, power):
         new_element = Element(coefficient=coefficient, power=power, next=None)
@@ -82,4 +69,15 @@ class BaseList:
             self.last = new_element
 
     def get(self, item):
-        return self.__getitem__(item)
+        if isinstance(item, int):
+            if item == 0:
+                return self.first
+            elif item == -1:    
+                return self.last
+            else:
+                element = self.first
+                counter = 0
+                while item !=counter:
+                    counter += 1
+                    element = element.next
+                return element
